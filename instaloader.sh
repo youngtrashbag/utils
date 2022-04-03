1c4538b93dc595a1f85b93ded6afc6cd62ae8cda
youngtrashbag <yuule.smeker@gmail.com>
2022-04-03 12:22:40 +0200

    refactor: instaloader example better

--- instaloader.sh
+++ instaloader.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-python -m instaloader --fast-update \
-    --sessionfile ./instaloader-personal_account_username.session \
-    --dirname-pattern='/media/images/ig_archive/{profile}' \
+USERNAME=kimkardashian
+PASSWORD=kimkardashian
+
+python3 -m instaloader \
+    --login $USERNAME --password $PASSWORD \
+    --dirname-pattern='/media/photo/ig_archive/{profile}' \
     --filename-pattern='{date_utc:%Y}/{date_utc:%m-%b}/{date_utc:%a-%d_%H-%M}' \
-    dg_bladee
+    draindler.page dg_bladee
