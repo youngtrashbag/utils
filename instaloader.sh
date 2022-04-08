11b60fc6c9c96fb27be3e57ead6e0400d4f3ccb2
youngtrashbag <yuule.smeker@gmail.com>
2022-04-08 09:41:15 +0200

    refactor: upgrade instaloader script

--- instaloader.sh
+++ instaloader.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-USERNAME=kimkardashian
-PASSWORD=kimkardashian
+USERNAME="kimkardashian"
+PASSWORD="kimkardashian"
+
+ARCHIVE_DIR="/media/images/ig_archive"
+LOG_DIR="${ARCHIVE_DIR}/logs"
+LOG_FILENAME="$(date +%Y-%m-%d_%H-%M-%S)_instaloader.log"
+
+mkdir -p "${LOG_DIR}"
 
 python3 -m instaloader \
-    --login $USERNAME --password $PASSWORD \
-    --dirname-pattern='/media/photo/ig_archive/{profile}' \
+    --login "${USERNAME}" --password "${PASSWORD}" \
+    --fast-update \
+    --dirname-pattern="${ARCHIVE_DIR}/{profile}" \
     --filename-pattern='{date_utc:%Y}/{date_utc:%m-%b}/{date_utc:%a-%d_%H-%M}' \
-    draindler.page dg_bladee
+    yunglean2001 dg_bladee \
+    >> "${LOG_DIR}/${LOG_NAME}"
